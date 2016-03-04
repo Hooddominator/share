82dea3b651300fff1fe0d1ae08d6363162a9a987
Hooddominator <hacktn4@gmail.com>
2016-03-04 22:21:45 +0100

    Create es_install.sh

---
+++ es_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## installs elasticsearch on RPi
+ESVER="2.2.0"
+
+aptitude update;
+aptitude install -y openjdk-8-jre-headless;
+mkdir /usr/share/elasticsearch;
+cd /usr/share/elasticsearch;
+wget "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ESVER}/elasticsearch-${ESVER}.tar.gz";
+sleep 1;
+tar -zxvf elasticsearch-${ESVER}.tar.gz;
+sleep 1;
+mv ./elasticsearch-${ESVER}/* .;
+rm -rf ./elasticsearch-${ESVER}*;
+useradd elasticsearch;
+mkdir /usr/share/elasticsearch/logs;
+mkdir /usr/share/elasticsearch/data;
+chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/;
+aptitude install -y supervisor;
+cd /etc/supervisor/conf.d/;
+wget https://raw.githubusercontent.com/Hooddominator/share/master/supervisor_es.conf;
+supervisorctl reread;
+supervisorctl update;
