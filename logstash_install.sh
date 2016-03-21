061711fc9a55350e834cd76fc61cdc3238a307c7
Hooddominator <hacktn4@gmail.com>
2016-03-21 14:37:40 +0100

    Create logstash_install.sh

---
+++ logstash_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## installs logstash on RPi
+LGSTVER="2.2.0";
+
+mkdir /usr/share/logstash;
+cd /usr/share/logstash;
+wget https://download.elastic.co/logstash/logstash/logstash-all-plugins-${LGSTVER}.tar.gz;
+sleep 1;
+tar -zxvf logstash-all-plugins-${LGSTVER}.tar.gz;
+mv logstash-all-plugins-${LGSTVER}/* .;
+rm -rf logstash-all-plugins-${LGSTVER}*;
+mkdir /usr/share/logstash/logs;
+mkdir /usr/share/logstash/data;
+chown -R elasticsearch:elasticsearch /usr/share/logstash/;
+cd /etc/supervisor/conf.d/;
+wget https://raw.githubusercontent.com/Hooddominator/share/master/supervisor_logstash.conf;
+supervisorctl reread;
+supervisorctl update;
